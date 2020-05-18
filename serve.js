2c31265e5e1b858f37eaa9403ea0240fa67a54c1
SHI ZHEN <zoe.zhenshi@gmail.com>
2020-05-18 20:08:17 +0300

    basic graphql

---
+++ serve.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const graphqlHttp = require('express-graphql')
+const app = express()
+const schema = require('./schema')
+app.use(
+	'/graphql',
+	graphqlHttp({
+		schema,
+		graphiql: true,
+	})
+)
+app.listen(4000)
+console.log('Listening')
