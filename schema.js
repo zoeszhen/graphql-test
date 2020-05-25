0b36b269ac672ade17b066d96102213838d8b76c
SHI ZHEN <zoe.zhenshi@gmail.com>
2020-05-25 11:26:15 +0300

    intigrate with google translate

--- schema.js
+++ schema.js
@@ -9,22 +9,43 @@ const {
 	GraphQLList,
 } = require('graphql')
 
+function translate(lang, str) {
+	// Google Translate API is a paid (but dirt cheap) service. This is my key
+	// and will be disabled by the time the video is out. To generate your own,
+	// go here: https://cloud.google.com/translate/v2/getting_started
+	const apiKey = 'AIzaSyApv1hai60CJU9pH9aL-iG8M7HTCNeu3aM'
+	const url =
+		'https://www.googleapis.com' +
+		'/language/translate/v2' +
+		'?key=' +
+		apiKey +
+		'&source=en' +
+		'&target=' +
+		lang +
+		'&q=' +
+		encodeURIComponent(str)
+	return fetch(url)
+		.then((response) => response.json())
+		.then((parsedResponse) => {
+			console.log('parsedResponse', JSON.stringify(parsedResponse, null, 2))
+			return parsedResponse.data.translations[0].translatedText
+		})
+}
+
 const BookType = new GraphQLObjectType({
 	name: 'Book',
 	description: '....',
 	fields: () => ({
 		title: {
 			type: GraphQLString,
-			resolve: (xml) => {
-				console.log(
-					'title',
-					JSON.stringify(
-						xml.GoodreadsResponse.author[0].books[0].book[0].title[0],
-						null,
-						2
-					)
-				)
-				return xml.GoodreadsResponse.author[0].books[0].book[0].title[0]
+			args: {
+				lang: {
+					type: GraphQLString,
+				},
+			},
+			resolve: (xml, args) => {
+				const title = xml.GoodreadsResponse.author[0].books[0].book[0].title[0]
+				return args.lang ? translate(args.lang, title) : title
 			},
 		},
 		isbn: {
